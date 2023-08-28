08ff7fe8762f2130a07302e8bb882c40ef3f7141
Rob Killick <robkillick@me.com>
2023-08-28 10:46:11 +0000

    update allowed hosts heroku app address

--- travelbuddy/settings.py
+++ travelbuddy/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rk-django-api-testing.herokuapp.com',
+ALLOWED_HOSTS = ['rk-django-api-testing-0f8531b1eb33.herokuapp.com',
                 '8000-rkillickdev-djangoapite-0qpi4ztxqjo.ws-eu104.gitpod.io']
 
 
